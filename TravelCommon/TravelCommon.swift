c630bc938f9237db340fa2d1735113b46fffca8e
Rohit T P <tprohit9@gmail.com>
2025-06-14 01:23:37 +0530

    Initial Commit

---
+++ TravelCommon/TravelCommon.swift
@@ -0,0 +1,9 @@
+//
+//  TravelCommon.swift
+//  TravelCommon
+//
+//  Created by Rohit T P on 14/06/25.
+//
+
+import Foundation
+
---
+++ TravelCommonTests/TravelCommonTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCommonTests.swift
+//  TravelCommonTests
+//
+//  Created by Rohit T P on 14/06/25.
+//
+
+import Testing
+@testable import TravelCommon
+
+struct TravelCommonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
